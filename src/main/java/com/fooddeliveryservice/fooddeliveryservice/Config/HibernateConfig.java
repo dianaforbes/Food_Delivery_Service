bb5c652990e85e123a64e8783fa562d5eb1f34b7
dianaforbes <diana.forbes96@gmail.com>
2019-05-16 18:25:16 -0400

    Initial commit

---
+++ src/main/java/com/fooddeliveryservice/fooddeliveryservice/Config/HibernateConfig.java
@@ -0,0 +1,11 @@
+package com.fooddeliveryservice.fooddeliveryservice.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({ "classpath:persistence.xml" })
+public class HibernateConfig {
+}
+
+
---
+++ src/main/java/com/fooddeliveryservice/fooddeliveryservice/Entity/User.java
@@ -0,0 +1,132 @@
+package com.fooddeliveryservice.fooddeliveryservice.Entity;
+
+import java.sql.Date;
+
+public class User {
+    private User user;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String userPassword;
+    private String permission;
+    private int addressId;
+    private String emailAddress;
+    private String phoneNumber;
+    java.sql.Date lastLogin;
+    java.sql.Date lastOrder;
+    private int subscriptionId;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Date getLastOrder() {
+        return lastOrder;
+    }
+
+    public void setLastOrder(Date lastOrder) {
+        this.lastOrder = lastOrder;
+    }
+
+    public int getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(int subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user=" + user +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", permission='" + permission + '\'' +
+                ", addressId=" + addressId +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", lastLogin=" + lastLogin +
+                ", lastOrder=" + lastOrder +
+                ", subscriptionId=" + subscriptionId +
+                '}';
+    }
+}
